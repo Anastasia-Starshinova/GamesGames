e479e75b192f95fc1805126584e9c43bb4993d5f
Anastasiia <starschinowa.ana@gmail.com>
2025-06-03 18:26:55 +0200

    edit gunicorn

--- app.py
+++ app.py
@@ -3587,15 +3587,13 @@ def show_game_player(message):
             bot.register_next_step_handler(message, show_game_player)
 
 
-# bot.polling(none_stop=True)
-# bot.infinity_polling(skip_updates=True)
-# bot.infinity_polling()
-
+#
 # if __name__ == "__main__":
-#     bot.polling(none_stop=True, skip_pending=True)
+#     bot.remove_webhook()
+#     bot.set_webhook(url=WEBHOOK_URL)
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
